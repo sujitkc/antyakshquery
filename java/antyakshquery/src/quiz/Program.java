3d9f25f7c5e06f4d3595d09566fc35e20fa185ed
sujitkc <sujitkc@gmail.com>
2020-05-26 15:14:10 +0530

    Fixed Makefile and manifest file and default data file paths in source code

--- java/antyakshquery/src/quiz/Program.java
+++ java/antyakshquery/src/quiz/Program.java
@@ -14,16 +14,20 @@ import javax.swing.JOptionPane;
 public class Program {
   private static ClassRoomFrame frame;
   public static void main(String[] args) {
-    File pfile = new File("../participants.txt");
-    File tfile = new File("../topics.txt");
+    String default_pfile = "participants.txt";
+    String default_tfile = "topics.txt";
+    File pfile = new File(default_pfile);
+    File tfile = new File(default_tfile);
     if(args.length != 0 && args[0].equals("-i")) {
-      JFileChooser pchooser = new JFileChooser("..");
+      JFileChooser pchooser = new JFileChooser(".");
+      pchooser.setDialogTitle("Select Participant File");
       int pretval = pchooser.showOpenDialog(Program.frame);
       if(pretval == JFileChooser.APPROVE_OPTION) {
         System.out.println("Chosen participants file = " + pfile);
         pfile = pchooser.getSelectedFile().getAbsoluteFile();
       }
-      JFileChooser tchooser = new JFileChooser("..");
+      JFileChooser tchooser = new JFileChooser(".");
+      tchooser.setDialogTitle("Select Topics File");
       int tretval = tchooser.showOpenDialog(Program.frame);
       if(tretval == JFileChooser.APPROVE_OPTION) {
         System.out.println("Chosen topics file = " + tfile);
@@ -31,11 +35,11 @@ public class Program {
       }
     }
     else {
-      System.out.println("Choosing default file ../participants.txt for participants ...");
-      System.out.println("Choosing default file ../topics.txt for topics ...");
+      System.out.println("Choosing default file " + default_pfile + " for participants ...");
+      System.out.println("Choosing default file " + default_tfile + " for topics ...");
     } 
     List<String> participants =  Program.getNamesFromFile(pfile);
-    if(participants.equals(null)) {
+    if(participants == null) {
       System.out.println("Empty participant list!");
       System.exit(0);
     }
